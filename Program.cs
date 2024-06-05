ea41d868c049ea8039b721ed0369cd2c15af8bb1
imrbpro <mrb5only>
2024-06-05 22:06:16 +0500

    Discounting Complete Ready Styling fixed

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace OSR_APP
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<IReadyService, ReadyService>();
+            builder.Services.AddTransient<IDiscountingService, DiscountingService>();
 
             builder.Services.AddMudServices();
             await builder.Build().RunAsync();
--- Services/Implementation/DiscountingService.cs
+++ Services/Implementation/DiscountingService.cs
@@ -1,13 +1,54 @@
-﻿using OSR_APP.Models;
+﻿using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using OSR_APP.Models;
 using OSR_APP.Services.Interface;
+using System.Net;
 
 namespace OSR_APP.Services.Implementation
 {
     public class DiscountingService : IDiscountingService
     {
-        public Task<Dictionary<string, IEnumerable<Discounting>>> GetDiscountingData()
+        private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
+        string baseApiURL = string.Empty;
+        Dictionary<string, string> dictionary;
+        public IEnumerable<Ready> ListMenuAddOns { get; set; }
+        public DiscountingService(HttpClient http, NavigationManager navigationManager, IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _http = http;
+            _navigationManager = navigationManager;
+            baseApiURL = $"{configuration["BackendApi"]}Discounting";
+        }
+        
+        private void SetDefaultHeaders(HttpRequestMessage request)
+        {
+            //request.Headers.Add("Authorization", ""); 
+            request.Headers.Add("Content-Type", "application/json");
+        }
+        public async Task<Dictionary<string, IEnumerable<Discounting>>> GetDiscountingData()
+        {
+            var dictionary = new Dictionary<string, IEnumerable<Discounting>>();
+            try
+            {
+                HttpResponseMessage result = await _http.GetAsync(baseApiURL);
+                var errorMessage = "";
+                if (result.StatusCode == HttpStatusCode.OK)
+                {
+                    var response = await result.Content.ReadAsStringAsync();
+                    var apiResponse = JsonConvert.DeserializeObject<ApiResponse<IEnumerable<Discounting>>>(response);
+                    dictionary.Add("discountingData", apiResponse.Data);
+                    return dictionary;
+                }
+                else
+                {
+                    errorMessage = await result.Content.ReadAsStringAsync();
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
     }
 }
